ca5d86ddb1a7cc06eb18aeb3b161a715c07376ec
Daryna <darynatymoshenko@gmail.com>
2023-09-28 15:04:05 +0300

    feat: create function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let numToString = n.toString();
+  let reverseString = '';
+
+  for(let i = numToString.length; i > 0; i--) {
+    reverseString += numToString[i - 1];
+  }
+  return parseInt(reverseString);
 }
